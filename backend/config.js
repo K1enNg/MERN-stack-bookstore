8040c78fb1b6f13625da425696ce6f707d4bccf0
Kinkin <hiimkien2702@gmail.com>
2024-11-14 08:50:09 -0500

    add mongoDB and mongoose to nodejs

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://root:Hiimkien2702**@book-store-mern.8celn.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-store-MERN';
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Hi there, Welcome to my very first MERN Stack App")
 });
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database')
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`)
+        });
+        
+    })
+    .catch((error) => {
+        console.log(error)
+    })
