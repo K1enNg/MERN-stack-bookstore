8775dc34d099b55b4d3cd229bd6273a62bd55294
Kinkin <hiimkien2702@gmail.com>
2024-11-14 20:08:08 -0500

    updated GET and POST methods

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,75 @@
-import express from 'express';
+import express, { response } from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { request } from 'http';
+import { count, error } from 'console';
+import { Book } from './models/bookModels.js';
+import { title } from 'process';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Hi there, Welcome to my very first MERN Stack App")
 });
 
+// Route for Save a new Book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        
+        const book = await Book.create(newBook);
+        
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
+
+//get data by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
 mongoose
     .connect(mongoDBURL)
     .then(() => {
