7af4c22184c0fdfcba20384ed9018f5f79a988d8
Kinkin <hiimkien2702@gmail.com>
2024-11-17 04:30:03 -0500

    create update and delete data functions

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/', (request, response) => {
     return response.status(234).send("Hi there, Welcome to my very first MERN Stack App")
 });
 
-// Route for Save a new Book
+//save data
 app.post('/books', async (request, response) => {
     try {
         if (
@@ -43,6 +43,7 @@ app.post('/books', async (request, response) => {
     }
 });
 
+//get all data
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
@@ -68,6 +69,56 @@ app.get('/books/:id', async (request, response) => {
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message })
+    }
+})
+
+//update data
+app.put('/books/:id',async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({message: 'Book not found'})
+        }
+
+        return response.status(200).send({message: 'Book updated successfully!'});
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//delete data
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({message: 'Book not found'})
+        }
+
+        return response.status(200).send({message: 'Book deleted successfully'})
+    }
+
+
+    catch (error) {
+        console.log(error.message)
+        response.status(500).send({message: error.message})
     }
 })
 mongoose
