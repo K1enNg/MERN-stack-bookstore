a1934d55eee4a39381bce8fe85b19c319e5af874
Kinkin <hiimkien2702@gmail.com>
2024-11-18 07:06:34 -0500

    display book list

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 //         allowHeaders: ['Content-Type']
 //     })
 // )
+app.use(cors());
 
 app.get('/', (request, response) => {
     console.log(request);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,10 +10,11 @@ import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 const Home = () => {
     const [books, setBooks] = useState([])
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         setLoading(true);
         axios
-            .get('http://localhost:5173/books')
+            .get('http://localhost:5555/books')
             .then((response) => {
                 setBooks(response.data.data)
                 setLoading(false);
@@ -22,7 +23,8 @@ const Home = () => {
                 console.log(error);
                 setLoading(false);
             })
-    }, [])
+    }, []);
+    
   return (
     <div className='p-4'>
         <div className='flex justify-between items-center'>
@@ -37,11 +39,47 @@ const Home = () => {
             <table className='w-full border-seperate border-spacing-2'>
                 <thead>
                     <tr>
-                        
+                        <th className='border border-slate-600 rounded-md'>No</th>
+                        <th className='border border-slate-600 rounded-md'>Title</th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>
+                            Author
+                        </th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>
+                            Publish Year
+                        </th>
+                        <th className='border border-slate-600 rounded-md'>Operation</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    {books.map((book, index) => (
+                        <tr key={book._id} className='h-8'>
+                            <td className='border border-slate-700 rounded-md text-center'>
+                                {index + 1}
+                            </td>
+                            <td className='border border-slate-700 rounded-md text-center'>
+                                {book.title}
+                            </td>
+                            <td className='border border-slate-700 rounded-md text-center max-hd:hidden'>
+                                {book.author}
+                            </td>
+                            <td className='border border-slate-700 rounded-md text-center max-hd:hidden'>
+                                {book.publishYear}
+                            </td>
+                            <td className='border border-slate-700 rounded-md text-center'>
+                                <div className='flex justify-center gap-x-4'>
+                                    <Link to={`/books/details/${book._id}`}>
+                                        <BsInfoCircle className='text-2x1 text-green-800'/>
+                                    </Link>
+                                    <Link to={`/books/edit/${book._id}`}>
+                                        <AiOutlineEdit className='text-2x1 text-yellow-600'/>
+                                    </Link>
+                                    <Link to={`/books/details/${book._id}`}>
+                                        <MdOutlineDelete className='text-2x1 text-red-600'/>
+                                    </Link>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         )}
